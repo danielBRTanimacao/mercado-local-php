1bde413790e83f23f57a5878aca380f288f697eb
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-19 10:48:34 -0300

    feat: creating a new css on index and init logical on data

--- resources/render.php
+++ resources/render.php
@@ -5,6 +5,7 @@ function renderHead($title) {
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <script src='https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'></script>
+    <link rel='stylesheet' href='/public/css/index.css'>
     <title>$title</title>
     </head>
     ";
