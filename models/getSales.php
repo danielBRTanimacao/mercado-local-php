02d5d12ce2ccc70f0cc63517f167761c9ab08797
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-22 17:53:00 -0300

    feat: created get sales

--- models/getSales.php
+++ models/getSales.php
@@ -1,7 +1,17 @@
 <?php 
+    # Primeiro indice e o valor atual o segundo e o anterior
+    # Exemplo [10, 2]
+    # O indice 0 da lista onde o valor é 10 vai ser o hoje o indice 1 no qual o valor é 2 representa o dia anterior.
+
     $db = new SQLite3(__DIR__ . '/../sales.db');
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        echo json_encode(["data" => [23, 12, 54, 24, 15, 10, 1]]);
+        echo json_encode(
+            [
+                "week" => [23, 12, 54, 24, 15, 10, 1], 
+                "day" => [10, 1], 
+                "month" => [105, 53]
+            ]
+        );
     } else {
         echo json_encode(["negado" => "Acesso negado!"]);
     }
--- public/js/finance.js
+++ public/js/finance.js
@@ -3,89 +3,78 @@ const ctxDay = document.getElementById("chartDayCircle");
 const ctxMonth = document.getElementById("chartMonthCircle");
 const errorTxt = document.querySelector("h2.error");
 
-const getSales = async () => {
-    try {
-        const response = await fetch(
-            "http://localhost:8080/models/getSales.php/",
-            { method: "GET" }
-        );
+const getSales = () => {
+    fetch("http://localhost:8080/models/getSales.php/", { method: "GET" })
+        .then((response) => response.json())
+        .then((sales) => {
+            const dataWeek = {
+                labels: [
+                    "Segunda",
+                    "Terça",
+                    "Quarta",
+                    "Quinta",
+                    "Sexta",
+                    "Sabado",
+                    "Domingo",
+                ],
+                datasets: [
+                    {
+                        label: "Vendas esta semana",
+                        data: sales.week,
+                        borderWidth: 1,
+                    },
+                ],
+            };
 
-        if (!response.ok) {
-            errorTxt.classList.remove("hidden");
-            errorTxt.innerHtml =
-                "Erro ao buscar os valores. Error: " + response.status;
-        }
+            const dataDay = {
+                labels: ["Hoje", "Anterior"],
+                datasets: [
+                    {
+                        label: "Ganhos",
+                        data: sales.day,
+                        backgroundColor: ["#101828", "rgb(212, 212, 212)"],
+                        hoverOffset: 4,
+                    },
+                ],
+            };
 
-        const json = response.json();
-        // console.log(json.value);
+            const dataMonth = {
+                labels: ["Mês atual", "Anterior"],
+                datasets: [
+                    {
+                        label: "Ganhos mensal",
+                        data: sales.month,
+                        backgroundColor: ["#101828", "rgb(212, 212, 212)"],
+                        hoverOffset: 4,
+                    },
+                ],
+            };
 
-        const dataWeek = {
-            labels: [
-                "Segunda",
-                "Terça",
-                "Quarta",
-                "Quinta",
-                "Sexta",
-                "Sabado",
-                "Domingo",
-            ],
-            datasets: [
-                {
-                    label: "Vendas esta semana",
-                    data: json,
-                    borderWidth: 1,
-                },
-            ],
-        };
+            new Chart(ctxDay, {
+                type: "doughnut",
+                data: dataDay,
+            });
 
-        const dataDay = {
-            labels: ["Hoje", "Anterior"],
-            datasets: [
-                {
-                    label: "Ganhos",
-                    data: [1, 15],
-                    backgroundColor: ["#101828", "rgb(212, 212, 212)"],
-                    hoverOffset: 4,
-                },
-            ],
-        };
-
-        const dataMonth = {
-            labels: ["Mês atual", "Anterior"],
-            datasets: [
-                {
-                    label: "Ganhos mensal",
-                    data: [105, 53],
-                    backgroundColor: ["#101828", "rgb(212, 212, 212)"],
-                    hoverOffset: 4,
-                },
-            ],
-        };
-
-        new Chart(ctxDay, {
-            type: "doughnut",
-            data: dataDay,
-        });
-
-        new Chart(ctxMonth, {
-            type: "doughnut",
-            data: dataMonth,
-        });
-        new Chart(ctx, {
-            type: "bar",
-            data: dataWeek,
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true,
+            new Chart(ctxMonth, {
+                type: "doughnut",
+                data: dataMonth,
+            });
+            new Chart(ctx, {
+                type: "bar",
+                data: dataWeek,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                        },
                     },
                 },
-            },
+            });
+        })
+        .catch((error) => {
+            errorTxt.classList.remove("hidden");
+            errorTxt.innerHtml = "Erro ao buscar os valores. Error: " + error;
         });
-    } catch (error) {
-        errorTxt.classList.remove("hidden");
-        errorTxt.innerHtml = "Erro ao buscar os valores. Error: " + error;
-    }
 };
 
 getSales();
